102d6418e3c6b2ad1ff0e2ed48936bc299581970
Kevin Cao <Kevin@kre.local>
2017-04-17 22:50:26 -0700

    hw2 commit

--- empdata.js
+++ empdata.js
@@ -1,16 +1,17 @@
-<<<<<<< HEAD
 {
   "data": [
     {
-      "name": "Garrett Winters",
+      "name": "Weiqun Cao",
+      "department": "Finance",
       "designation": "Analyst",
       "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
+      "joining_date": "2014/08/01",
+      "office": "Seattle",
       "extension": "8422"
     },
     {
       "name": "Brielle Williamson",
+      "department": "IT",
       "designation": "Integration Specialist",
       "salary": "$372,000",
       "joining_date": "2012/12/02",
@@ -19,6 +20,7 @@
     },
     {
       "name": "Ashton Cox",
+      "department": "IT",
       "designation": "Junior Technical Author",
       "salary": "$86,000",
       "joining_date": "2009/01/12",
@@ -27,6 +29,7 @@
     },
     {
       "name": "Airi Satou",
+      "department": "Finance",
       "designation": "Accountant",
       "salary": "$162,700",
       "joining_date": "2008/11/28",
@@ -35,6 +38,7 @@
     },
     {
       "name": "Caesar Vance",
+      "department": "Sales",
       "designation": "Pre-Sales Support",
       "salary": "$106,450",
       "joining_date": "2011/12/12",
@@ -43,6 +47,7 @@
     },
     {
       "name": "Shad Decker",
+      "department": "Sales",
       "designation": "Regional Director",
       "salary": "$183,000",
       "joining_date": "2008/11/13",
@@ -51,6 +56,7 @@
     },
     {
       "name": "Cedric Kelly",
+      "department": "IT",
       "designation": "Senior Javascript Developer",
       "salary": "$433,060",
       "joining_date": "2012/03/29",
@@ -59,6 +65,7 @@
     },
     {
       "name": "Haley Kennedy",
+      "department": "Marketing",
       "designation": "Senior Marketing Designer",
       "salary": "$313,500",
       "joining_date": "2012/12/18",
@@ -67,6 +74,7 @@
     },
     {
       "name": "Colleen Hurst",
+      "department": "IT",
       "designation": "Javascript Developer",
       "salary": "$205,500",
       "joining_date": "2009/09/15",
@@ -75,6 +83,7 @@
     },
     {
       "name": "Dai Rios",
+      "department": "HR",
       "designation": "Personnel Lead",
       "salary": "$217,500",
       "joining_date": "2012/09/26",
@@ -83,6 +92,7 @@
     },
     {
       "name": "Herrod Chandler",
+      "department": "Sales",
       "designation": "Sales Assistant",
       "salary": "$137,500",
       "joining_date": "2012/08/06",
@@ -91,6 +101,7 @@
     },
     {
       "name": "Rhona Davidson",
+      "department": "IT",
       "designation": "Integration Specialist",
       "salary": "$327,900",
       "joining_date": "2010/10/14",
@@ -99,6 +110,7 @@
     },
     {
       "name": "Sonya Frost",
+      "department": "IT",
       "designation": "Software Engineer",
       "salary": "$103,600",
       "joining_date": "2008/12/13",
@@ -107,6 +119,7 @@
     },
     {
       "name": "Jena Gaines",
+      "department": "Admin",
       "designation": "Office Manager",
       "salary": "$90,560",
       "joining_date": "2008/12/19",
@@ -115,6 +128,7 @@
     },
     {
       "name": "Quinn Flynn",
+      "department": "Admin",
       "designation": "Support Lead",
       "salary": "$342,000",
       "joining_date": "2013/03/03",
@@ -122,129 +136,4 @@
       "extension": "9497"
     }
   ]
-=======
-{
-  "data": [
-    {
-      "name": "Garrett Winters",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
-    },
-    {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
-    },
-    {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
-    },
-    {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
-    },
-    {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
-    },
-    {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
-    },
-    {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
-    },
-    {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
-    },
-    {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
-    },
-    {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
-    },
-    {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
-    },
-    {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
-    },
-    {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
-    },
-    {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
-    },
-    {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
-    }
-  ]
->>>>>>> 51f1321b957d3ebb96db7364945a03881911af5a
 }

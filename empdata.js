105138294ef828f5f5317cd5b6015d07fc71d6db
Kevin Cao <Kevin@kre.local>
2017-04-17 21:34:44 -0700

    hw2 commit

--- empdata.js
+++ empdata.js
@@ -2,6 +2,7 @@
   "data": [
     {
       "name": "Garrett Winters",
+      "department": "Finance",
       "designation": "Accountant",
       "salary": "$170,750",
       "joining_date": "2011/07/25",
@@ -10,6 +11,7 @@
     },
     {
       "name": "Brielle Williamson",
+      "department": "IT",
       "designation": "Integration Specialist",
       "salary": "$372,000",
       "joining_date": "2012/12/02",
@@ -18,6 +20,7 @@
     },
     {
       "name": "Ashton Cox",
+      "department": "IT",
       "designation": "Junior Technical Author",
       "salary": "$86,000",
       "joining_date": "2009/01/12",
@@ -26,6 +29,7 @@
     },
     {
       "name": "Airi Satou",
+      "department": "Finance",
       "designation": "Accountant",
       "salary": "$162,700",
       "joining_date": "2008/11/28",
@@ -34,6 +38,7 @@
     },
     {
       "name": "Caesar Vance",
+      "department": "Sales",
       "designation": "Pre-Sales Support",
       "salary": "$106,450",
       "joining_date": "2011/12/12",
@@ -42,6 +47,7 @@
     },
     {
       "name": "Shad Decker",
+      "department": "Sales",
       "designation": "Regional Director",
       "salary": "$183,000",
       "joining_date": "2008/11/13",
@@ -50,6 +56,7 @@
     },
     {
       "name": "Cedric Kelly",
+      "department": "IT",
       "designation": "Senior Javascript Developer",
       "salary": "$433,060",
       "joining_date": "2012/03/29",
@@ -58,6 +65,7 @@
     },
     {
       "name": "Haley Kennedy",
+      "department": "Marketing",
       "designation": "Senior Marketing Designer",
       "salary": "$313,500",
       "joining_date": "2012/12/18",
@@ -66,6 +74,7 @@
     },
     {
       "name": "Colleen Hurst",
+      "department": "IT",
       "designation": "Javascript Developer",
       "salary": "$205,500",
       "joining_date": "2009/09/15",
@@ -74,6 +83,7 @@
     },
     {
       "name": "Dai Rios",
+      "department": "HR",
       "designation": "Personnel Lead",
       "salary": "$217,500",
       "joining_date": "2012/09/26",
@@ -82,6 +92,7 @@
     },
     {
       "name": "Herrod Chandler",
+      "department": "Sales",
       "designation": "Sales Assistant",
       "salary": "$137,500",
       "joining_date": "2012/08/06",
@@ -90,6 +101,7 @@
     },
     {
       "name": "Rhona Davidson",
+      "department": "IT",
       "designation": "Integration Specialist",
       "salary": "$327,900",
       "joining_date": "2010/10/14",
@@ -98,6 +110,7 @@
     },
     {
       "name": "Sonya Frost",
+      "department": "IT",
       "designation": "Software Engineer",
       "salary": "$103,600",
       "joining_date": "2008/12/13",
@@ -106,6 +119,7 @@
     },
     {
       "name": "Jena Gaines",
+      "department": "Admin",
       "designation": "Office Manager",
       "salary": "$90,560",
       "joining_date": "2008/12/19",
@@ -114,6 +128,7 @@
     },
     {
       "name": "Quinn Flynn",
+      "department": "Admin",
       "designation": "Support Lead",
       "salary": "$342,000",
       "joining_date": "2013/03/03",
